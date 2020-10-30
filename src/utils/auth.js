aac627ada88c823670de66b648822533425b7e37
梁锦东 <674964222@qq.com>
2020-10-30 16:35:11 +0800

    添加storage方法

--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,4 +1,5 @@
 const tokenKey = '__gating__token'
+const sessionStorage = window.sessionStorage
 
 export function getToken() {
   return getStorage(tokenKey)
@@ -13,6 +14,9 @@ export function removeToken() {
 }
 
 export function getStorage(key) {
+  if (typeof key !== 'string') {
+    return undefined
+  }
   try {
     return JSON.parse(sessionStorage.getItem(key))
   } catch (err) {
@@ -26,4 +30,18 @@ export function setStorage(key, data) {
 
 export function removeStorage(TokenKey) {
   return sessionStorage.removeItem(TokenKey)
+}
+
+export function getAllStorage() {
+  const all = {}
+  for (let i = 0; i < sessionStorage.length; i++) {
+    const key = sessionStorage.key(i)
+    const val = getStorage(key)
+    all[key] = val
+  }
+  return all
+}
+
+export function clearStorage() {
+  return sessionStorage.clear()
 }
