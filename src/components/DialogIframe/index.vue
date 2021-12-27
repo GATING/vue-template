ed180c1cc853f3c8e45c1d7c461e790bd18d5ec2
txy <812174597@qq.com>
2021-12-27 17:56:53 +0800

    feat: 新增 tag 和iframe 组件

---
+++ src/components/DialogIframe/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <el-dialog :title="title" :visible.sync="setVisible" :width="w" :append-to-body="appendBody">
+    <iframe :src="url" style="width: 100%" :style="{ height: iframeH }" frameborder="0" />
+  </el-dialog>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      default: '预览'
+    },
+    w: {
+      type: String,
+      default: '80%'
+    },
+    url: {
+      type: String,
+      default: ''
+    },
+    visible: {
+      type: Boolean,
+      default: false
+    },
+    iframeH: {
+      type: String,
+      default: '700px'
+    },
+    appendBody: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {}
+  },
+  computed: {
+    setVisible: {
+      get() {
+        return this.visible
+      },
+      set(val) {
+        this.$emit('update:visible', val)
+      }
+    }
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
