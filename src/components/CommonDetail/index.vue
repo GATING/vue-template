c61c0fc319ae0252a3946d8c177141439216a648
txy <812174597@qq.com>
2021-12-28 11:44:21 +0800

    feat: 新增 一个commonDetail 组件
    新增 tag 和iframe 组件demo 说明

---
+++ src/components/CommonDetail/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="" v-if="list.length > 0">
+    <el-row v-for="(item, index) in list" :key="index">
+      <el-col v-bind="col.layout" v-for="(col, colIndex) in item" :key="colIndex">
+        <div class="text" :style="col.textStyle">{{ col.text }}</div>
+        <div class="con" :style="col.conStyle">{{ col.con }}</div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    list: {
+      type: Array,
+      default() {
+        return []
+      }
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+.text,
+.con {
+  display: inline-block;
+}
+</style>
--- src/views/demo/test.vue
+++ src/views/demo/test.vue
@@ -26,15 +26,19 @@
         <el-button @click="visible = true">弹窗iframe内容</el-button>
       </el-dialog>
     </div>
-    <div></div>
+    <div>
+      <h1>commonDetail</h1>
+      <common-detail :list="detailList" />
+    </div>
   </div>
 </template>
 
 <script>
 import DialogIframe from '@comp/DialogIframe/index.vue'
 import DictTag from '@comp/DictTag/index.vue'
+import CommonDetail from '@comp/CommonDetail/index.vue'
 export default {
-  components: { DialogIframe, DictTag },
+  components: { DialogIframe, DictTag, CommonDetail },
   data() {
     return {
       // DictTag
@@ -56,7 +60,31 @@ export default {
       url: 'http://www.baidu.com',
       outerVisible: false,
       visible: false,
-      iframeH: '700px'
+      iframeH: '700px',
+      // commonDetail
+      detailList: [
+        [
+          {
+            text: 'cpu',
+            con: '2.5g',
+            layout: {
+              md: 5
+            },
+            textStyle: {
+              width: '100px',
+              textAlign: 'left'
+            },
+            conStyle: {}
+          },
+          {
+            text: '系统',
+            con: 'windows',
+            layout: {
+              md: 5
+            }
+          }
+        ]
+      ]
     }
   },
   mounted() {},
